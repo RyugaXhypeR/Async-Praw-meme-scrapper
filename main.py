070098b07cb7216257d75951422f0538421509b4
RyugaXhypeR <91488988+RyugaXhypeR@users.noreply.github.com>
2022-01-24 14:16:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import asyncpraw
+import asyncio
+import requests
+import random
+
+SUBREDDIT = ''
+TOTAL_MEMES = 
+FOLDER_NAME = ''
+VALID_SYM = ''
+LIMIT = 
+
+
+async def main():
+    reddit = asyncpraw.Reddit(
+        client_id='',
+        client_secret='',
+        username='',
+        user_agent='',
+    )
+
+    subreddit = await reddit.subreddit(SUBREDDIT)
+    lim = subreddit.hot(limit=TOTAL_MEMES)
+    all_sub = []
+
+    async for i in lim:
+        all_sub.append(i)
+
+    submission = random.sample(all_sub, LIMIT)
+    for i in range(LIMIT):
+
+        title = submission[i].title
+        url = submission[i].url
+
+        for j in title:
+            if j.lower() not in VALID_SYM:
+                title = title.replace(j, '-')
+
+        print(f"{i+1}. {url}")
+        request_url = requests.get(url).content
+        with open(f'{FOLDER_NAME}/{title}.jpg', 'wb+') as f:
+            f.write(request_url)
+
+        await reddit.close()
+
+#event loop
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
